fc21906fa21261c6d2d50049c758b719a590b75d
DrexHD <nicknamedrex@gmail.com>
2020-08-30 22:42:19 +0200

    Update SpawnRestrictionMixin.java

--- src/main/java/me/drexhd/itsmine/mixin/SpawnRestrictionMixin.java
+++ src/main/java/me/drexhd/itsmine/mixin/SpawnRestrictionMixin.java
@@ -2,11 +2,12 @@ package me.drexhd.itsmine.mixin;
 
 import me.drexhd.itsmine.ClaimManager;
 import me.drexhd.itsmine.claim.Claim;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.SpawnRestriction;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.WorldAccess;
+import net.minecraft.world.ServerWorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,12 +18,12 @@ import java.util.Random;
 @Mixin(SpawnRestriction.class)
 public class SpawnRestrictionMixin {
 	@Inject(method = "canSpawn", at = @At("HEAD"), cancellable = true)
-	private static void canSpawnInClaim(EntityType<?> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random, CallbackInfoReturnable<Boolean> cir) {
+	private static <T extends Entity> void canSpawnInClaim(EntityType<T> type, ServerWorldAccess serverWorldAccess, SpawnReason spawnReason, BlockPos pos, Random random, CallbackInfoReturnable<Boolean> cir) {
 		if (ClaimManager.INSTANCE == null) {
 			cir.setReturnValue(false);
 			return;
 		}
-		Claim claim = ClaimManager.INSTANCE.getClaimAt(pos, world.getDimension());
+		Claim claim = ClaimManager.INSTANCE.getClaimAt(pos, serverWorldAccess.toServerWorld().getDimension());
 		if(claim != null && !claim.flagManager.hasFlag("mob_spawn")) {
 		    cir.setReturnValue(false);
         }
